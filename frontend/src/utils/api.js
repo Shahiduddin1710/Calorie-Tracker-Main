f4a8939cb977bfd177cd0cbbdabc64081eeeda3c
Shahiduddin1710 <shahiduddin153@gmail.com>
2026-03-18 21:38:54 +0530

    first commit

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  timeout: 15000
+})
+
+// ✅ ADD THIS - attaches token to every request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('ct_token')
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('ct_token')
+      delete api.defaults.headers.common['Authorization']
+      window.location.href = '/signin'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
